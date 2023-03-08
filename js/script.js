1329abb7166b4ca1554fc2404e9a87d3b269957c
Miguel Andrade <101271954+MiguelHAndrade@users.noreply.github.com>
2023-03-08 17:56:42 +0000

    v1
    1st Display Update and new pictures

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// window.alert('Hello World')
+
+// function ampliarImagem(img) {
+//     img.style.width = "500px"; // altera a largura da imagem para 500px
+//     img.style.height = "auto"; // ajusta a altura automaticamente
+//   }
+
+function openWindow(imagemUrl) {
+window.open(imagemUrl, "_blank", "width=100vh,height=auto"); // abre a imagem em uma nova janela com o tamanho de 800x600
+}
+
