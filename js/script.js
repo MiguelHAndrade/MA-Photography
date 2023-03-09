2edf995968fc2c8b5f16115ab82c3161efa041b6
Miguel Andrade <101271954+MiguelHAndrade@users.noreply.github.com>
2023-03-09 12:57:19 +0000

    Responsive
    Responsive - Update
    Open - IMG full screed

--- js/script.js
+++ js/script.js
@@ -9,3 +9,80 @@ function openWindow(imagemUrl) {
 window.open(imagemUrl, "_blank", "width=100vh,height=auto"); // abre a imagem em uma nova janela com o tamanho de 800x600
 }
 
+// ////////////////////////////////// Open Full Screen Mode
+// var img = document.getElementById("fullscreenImg");
+// var closeBtn = document.getElementById("closeBtn");
+
+// function openFullscreen() {
+//   if (img.requestFullscreen) {
+//     img.requestFullscreen();
+//   } else if (img.webkitRequestFullscreen) { /* Safari */
+//     img.webkitRequestFullscreen();
+//   } else if (img.msRequestFullscreen) { /* IE11 */
+//     img.msRequestFullscreen();
+//   }
+//   closeBtn.style.display = "block"; // Exibe o botão "close"
+//   img.style.cursor = "auto";
+// }
+
+// function closeFullscreen() {
+//   if (document.exitFullscreen) {
+//     document.exitFullscreen();
+//   } else if (document.webkitExitFullscreen) { /* Safari */
+//     document.webkitExitFullscreen();
+//   } else if (document.msExitFullscreen) { /* IE11 */
+//     document.msExitFullscreen();
+//   }
+//   closeBtn.style.display = "none"; // Esconde o botão "close"
+// }
+
+// closeBtn.addEventListener("click", closeFullscreen); // Adiciona evento de clique ao botão "close"
+// /////////////////////////////////////////////////////////
+
+var imgs = document.querySelectorAll(".fullscreen-img");
+
+function openFullscreen(img) {
+  if (img.requestFullscreen) {
+    img.requestFullscreen();
+  } else if (img.webkitRequestFullscreen) { /* Safari */
+    img.webkitRequestFullscreen();
+  } else if (img.msRequestFullscreen) { /* IE11 */
+    img.msRequestFullscreen();
+  }
+  img.style.cursor = "auto";
+
+  closeFullscreen()
+}
+
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
+}
+
+// Adiciona evento de clique em cada imagem
+for (var i = 0; i < imgs.length; i++) {
+  imgs[i].addEventListener("click", function() {
+    openFullscreen(this);
+  });
+}
+
+//////////////////////////////////
+
+var myDiv = document.querySelectorAll(".card");
+
+myDiv.addEventListener('touchstart', function() {
+  myDiv.classList.add('active');
+});
+
+myDiv.addEventListener('touchmove', function() {
+  myDiv.classList.add('active');
+});
+
+myDiv.addEventListener('touchend', function() {
+  myDiv.classList.remove('active');
+});
